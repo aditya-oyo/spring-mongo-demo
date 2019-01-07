dc039dfa9806109d1d29687f0455d2c020f070eb
oyo <aditya1@oyorooms.com>
2019-01-07 15:27:12 +0530

    initial commit

---
+++ src/main/java/com/springmongo/demo/springmongodemo/repository/PetsRepository.java
@@ -0,0 +1,14 @@
+package com.springmongo.demo.springmongodemo.repository;
+
+import com.springmongo.demo.springmongodemo.model.Pets;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface PetsRepository extends MongoRepository<Pets,String> {
+    Pets findBy_id(ObjectId _id);
+    List<Pets> findAll();
+    Pets save(Pets p);
+    void delete(Pets p);
+}
